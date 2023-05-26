19783926c3048fd9ee7ea3bf31fe8ef7b67486e3
loquillas <lucasniederle02@gmail.com>
2023-05-25 21:15:36 -0300

    hola

--- dns.py
+++ dns.py
@@ -1,9 +1,9 @@
-import scapy
-from scapy.all import DNS, DNSQR, IP, sr1, UDP
+#import scapy
+from scapy.all import *
 
-dns_req = IP(dst='8.8.8.8')/
-    UDP(dport=53)/
-    DNS(rd=1, qd=DNSQR(qname='www.thepacketgeek.com'))
+dns_req = IP(dst='8.8.8.8')
+UDP(dport=53)
+DNS(rd=1, qd=DNSQR(qname='www.thepacketgeek.com'))
 answer = sr1(dns_req, verbose=0)
 
 import dns.message
