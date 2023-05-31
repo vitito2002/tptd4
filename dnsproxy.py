df219edd56120dcd8d04cfa7da2438f9245af4e4
Sofi Kutter <sofikutter01@gmail.com>
2023-05-30 23:04:17 -0300

    NUEVOOO POSTAAA

--- dnsproxy.py
+++ dnsproxy.py
@@ -3,7 +3,7 @@ from socket import *
 import sys
 from scapy.all import *
 import argparse
-from scapy.layers.dns import DNS, DNSQR, UDP, IP
+from scapy.layers.dns import DNS, DNSQR, UDP, IP, DNSRR
 
 #funcion auxiliar para procesar el argumento predeterminado de parser 
 def config_predet(lista:list):
@@ -31,8 +31,10 @@ class DNSProxy:
         #UDP(sport=RandShort(), dport=53):se establece el puerto de origen aleatoriamente.
         #client_query[DNS]:accede al campo DNS, viendo los registros
 
-        if client_query[DNSQR].qname.decode() in mapred: #si esta mapeada la query se genera la respuesta predeterminada
-            dns_response = IP() / UDP() / DNS(rd=1, id=client_query[DNS].id, qr=1, qdcount=1, ancount=1, qd=DNSQR(qname=client_query[DNSQR].qname), an=DNSRR(rrname=client_query[DNSQR].qname, ttl=10, rdata=mapred[client_query[DNSQR].qname]))
+        if client_query[DNSQR].qname.decode() in self.mapred: #si esta mapeada la query se genera la respuesta predeterminada
+            dns_response = IP() / UDP() / DNS(rd=1, id=client_query[DNS].id, qr=1, qdcount=1, ancount=1, 
+                                              qd=DNSQR(qname=client_query[DNSQR].qname), an=DNSRR(rrname=client_query[DNSQR].qname, 
+                                              ttl=10, rdata=self.mapred[client_query[DNSQR].qname]))   
         else:
             dns_response = sr1(dns_query, verbose=0) #envía la consulta al servidor DNS remoto y espera la respuesta.
         
@@ -46,9 +48,9 @@ class DNSProxy:
             data, address = self.server_socket.recvfrom(4096)
             client_query = IP(data)
             client_address = address
-            socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-            self.handle_dns_query(client_query, remote_dns_ip, mapred)
+            self.handle_dns_query(client_query)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="DNS Proxy Server")
